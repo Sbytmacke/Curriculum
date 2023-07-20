b2ff8a45465694229e078190f938b27ae84a7687
Angel M.Chivite <sbytmacke@gmail.com>
2023-07-20 15:20:07 +0200

    Organize folder and main web mobile

---
+++ src/js/events.js
@@ -0,0 +1,5 @@
+// Clase "always-hover" al elemento con id="button-to-hover" para identificar el elemento y realizar el evento
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("button-to-hover");
+  button.classList.add("always-hover");
+});
