9974ebd9dc160595efaffb750e0eee0f9d8aae82
Angel M.Chivite <sbytmacke@gmail.com>
2023-09-04 13:05:33 +0200

    [Fix] (Bottom layout width fix)

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -38,6 +38,12 @@ const projectsList = [
     description: "Descripción del proyecto 2",
     link: "",
   },
+  {
+    title: "Coming Soon...",
+    image: images.comingSoon,
+    description: "Descripción del proyecto 2",
+    link: "",
+  },
 ];
 
 function Projects() {
